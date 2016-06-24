746ad7ffbbf2fc78a35f5cf603a136f28995fd54
emisael <emisaelcavalcante@hotmail.com>
2016-06-24 17:08:05 -0300

    Alteração Fatorial.java - master

--- Fatorial.java
+++ Fatorial.java
@@ -2,7 +2,7 @@
 		// Versão interativa do cálculo fatorial
 		public int calcularFatorial(int n){
 			int resultado = 1;
-			if(n > ){
+			if(n > 2){ //Se n for maior ou igual a 2
 				for (int i = 2; i <= n; i++){
 					resultado *= i;
 					return resultado;
