2eb579c77900523045ba3ddc953a305de8c1f68f
emisael <emisaelcavalcante@hotmail.com>
2016-06-24 12:46:57 -0300

    Concerto em Fatorial.java

--- Fatorial.java
+++ Fatorial.java
@@ -2,12 +2,11 @@
 		// Versão interativa do cálculo fatorial
 		public int calcularFatorial(int n){
 			int resultado = 1;
-			if (i > 0){
-				for (int i = 2; i <= n; i++){
-					resultado *= i;
-					return resultado;
-			}	}
+			for (int i = 2; i <= n; i++){
+				resultado *= i;
+				return resultado;
+					
+			}
 		}
 	}
 	
-}
