6d2ec648d5c9247d07eba469b0153e6b26ac33e5
emisael <emisaelcavalcante@hotmail.com>
2016-06-24 12:22:03 -0300

    Alteração em Fatorial.java da branch remov

--- Fatorial.java
+++ Fatorial.java
@@ -1,11 +1,12 @@
-public class Fatorial{
+﻿public class Fatorial{
 		// Versão interativa do cálculo fatorial
 		public int calcularFatorial(int n){
 			int resultado = 1;
-			for (int i = 2; i <= n; i++){
-				resultado *= i;
-				return resultado;
-			}
+			if (i > 0){
+				for (int i = 2; i <= n; i++){
+					resultado *= i;
+					return resultado;
+			}	}
 		}
 	}
 	
